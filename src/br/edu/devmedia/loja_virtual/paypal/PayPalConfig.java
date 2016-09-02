7dfb9e1a40483f7f707916631d89db33274aa5b4
matteus <matteus_h@hotmail.com>
2016-09-01 22:07:53 -0300

    Alterando configurações

--- src/br/edu/devmedia/loja_virtual/paypal/PayPalConfig.java
+++ src/br/edu/devmedia/loja_virtual/paypal/PayPalConfig.java
@@ -7,8 +7,8 @@ import br.edu.devmedia.loja_virtual.util.WebServiceUtil;
 
 public class PayPalConfig {
 	
-	public static final String PAYPAL_CLIENT_ID = "AdlXmRzVHgpvqPvAjOqPyR2SXpVKpfSKBrm-V7k3ynvRfUfSzOE3OEhNQzl1psmJMoScunyEUdZnr0Zc";
-	public static final String SENHA = "EPnMEfmdX6gXg_mofBDbJ7T4p22IbogioTFwlYYGa8YkLJQ8wsHLlDRyypTWyvqGl52uD_A1pn1hthEM";
+	public static final String PAYPAL_CLIENT_ID = "";
+	public static final String SENHA = "";
 	
 	public static final String PAYPAL_ENV = PayPalConfiguration.ENVIRONMENT_SANDBOX;
 	public static final String PAYPAL_INTENT = PayPalPayment.PAYMENT_INTENT_SALE;
